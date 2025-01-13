c64d353dcfc7655e25ac1395ab9b304f6440def2
AlexandraButu <alexandra-gabriela.butu@student.tuiasi.ro>
2025-01-13 18:24:52 +0200

    added game logic

--- GameState.cs
+++ GameState.cs
@@ -10,6 +10,7 @@ namespace ProiectIA
     {
         public List<Character> RemainingCharacters { get; set; }
         public List<string> AvailableQuestions { get; set; }
+        public List<string> AskedQuestions { get; set; }
 
         public GameState(List<Character> characters, List<string> questions)
         {
@@ -17,6 +18,31 @@ namespace ProiectIA
             AvailableQuestions = new List<string>(questions);
         }
 
+        public void RemoveQuestion(string question)
+        {
+            if (AvailableQuestions.Contains(question))
+            {
+                AvailableQuestions.Remove(question);
+                AskedQuestions.Add(question);
+            }
+        }
+
+        public void UpdateRemainingCharacters(Func<Character, bool> predicate)
+        {
+            RemainingCharacters = RemainingCharacters.Where(predicate).ToList();
+        }
+
+        public void Reset(List<Character> characters, List<string> questions)
+        {
+            RemainingCharacters = new List<Character>(characters);
+            AvailableQuestions = new List<string>(questions);
+            AskedQuestions.Clear();
+        }
+
+        public bool IsGameOver()
+        {
+            return RemainingCharacters.Count <= 1;
+        }
     }
 
 }
